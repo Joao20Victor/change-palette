2d2381cc4005d1df35438ae3d37e4da8e01c1892
Joao20Victor <joao.super.sayajin.007@gmail.com>
2023-09-03 23:41:24 -0300

    avançando na página

--- src/app/components/algo.tsx
+++ src/app/components/algo.tsx
@@ -3,13 +3,13 @@
 import { ReactNode} from 'react'
 
 interface propsAlgo  {
-  style: string,
+  style: any,
   children: ReactNode
 }
 
 export default function Algo(props:propsAlgo){
   return(
-    <div className="text-slate-400 h-1/5" style={{background: props.style}} >
+    <div className="text-slate-400 h-full w-1/5" style={{background: props.style}} >
       {props.children}
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,87 @@
-
-
+'use client'
+import {useState} from 'react'
 import Algo from "./components/algo"
 
 export default function Home() {
+  const palettes = [
+    {
+      Color1: "#F2F2F2",
+      Color2: "#BFBFBF",
+      Color3: "#8C8C8C",
+      Color4: "#595959",
+      Color5: "#262626",
+    },
+    {
+      Color1: "#F29F05",
+      Color2: "#F27405",
+      Color3: "#F24405",
+      Color4: "#F20505",
+      Color5: "#0D0D0D",
+    },
+    {
+      Color1: "#212940",
+      Color2: "#245CA6",
+      Color3: "#2594D9",
+      Color4: "#27B9F2",
+      Color5: "#27CDF2",
+    },
+    {
+      Color1: "#D744E4",
+      Color2: "#57005E",
+      Color3: "#CC00DE",
+      Color4: "#591C5E",
+      Color5: "#9D00AB",
+    },
+    {
+      Color1: "#E0A5A3",
+      Color2: "#DA7E78",
+      Color3: "#CE534E",
+      Color4: "#BB0102",
+      Color5: "#8E1715",
+    },
+    {
+      Color1: "#A69668",
+      Color2: "#A69D85",
+      Color3: "#8C6C30",
+      Color4: "#59380E",
+      Color5: "#401E01",
+    },
+    {
+      Color1: "#A65C32",
+      Color2: "#73412F",
+      Color3: "#BF6E50",
+      Color4: "#D9C1B8",
+      Color5: "#BF6854",
+    },
+    {
+      Color1: "#ADBF7E",
+      Color2: "#737F54",
+      Color3: "#E8FFA8",
+      Color4: "#3A402A",
+      Color5: "#D0E597",
+    },
+  ]
+  const [color1, setColor1] = useState("#BFBFBF")
+  const [color2, setColor2] = useState("#595959")
+  const [color3, setColor3] = useState("#BFBFBF")
+  const [color4, setColor4] = useState("#595959")
+  const [color5, setColor5] = useState("#BFBFBF")
   return (
-    <div>
-      <Algo style="black" >
-        desga
+    <div className='flex h-screen' >
+      <Algo style={color1} >
+        {color1}
+      </Algo>
+      <Algo style={color2} >
+        {color2}
+      </Algo>
+      <Algo style={color3} >
+        {color3}
+      </Algo>
+      <Algo style={color4} >
+        {color4}
+      </Algo>
+      <Algo style={color5} >
+        {color5}
       </Algo>
     </div>
   )
